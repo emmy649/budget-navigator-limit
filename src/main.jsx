58dfb6e896e50e7047d6263744668d3cec2614d5
emmy649 <em.damyanova@gmail.com>
2025-10-04 14:55:07 +0300

    fix: iOS PWA start_url/scope + apple-touch-icon

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ createRoot(document.getElementById('root')).render(
 )
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js").catch(console.error);
+    navigator.serviceWorker.register(`${import.meta.env.BASE_URL}sw.js`);
+
   });
 }
